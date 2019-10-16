160bd37fac1ea22e8253c76b2aaa053e0659a01b
bandrey <bitkin.and@mail.ru>
2019-10-16 20:22:28 +0300

    Add title

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv)
 	double rad, area, length;
 	rad=15.9;
 	area_circle(rad, area, length);
+	std::cout <<"Circle, radius:"<< rad << std::endl;
 	std::cout <<"Area:"<< area << std::endl;
 	std::cout <<"Length:"<< length << std::endl;
 return 0;
